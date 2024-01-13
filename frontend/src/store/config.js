eedb753a537010003e0b0c738b02cb7303559de3
sashimizakana <atto_999@hotmail.com>
2024-01-13 23:48:51 +0900

    localStorageへの保存ができていなかったので修正

--- frontend/src/store/config.js
+++ frontend/src/store/config.js
@@ -4,28 +4,32 @@ import { useLocalStorage } from "@vueuse/core";
 export const useConfigStore = defineStore("config", {
   state: () => {
     return {
-      config:null
-    }
+      config: null,
+    };
   },
-  getters:{
-    get(state){
-      return (key,defaultValue) => state.config[key] ?? defaultValue;
-    }
-  },
-  actions:{
-    load(){
-      const config = useLocalStorage("config",{
+  getters: {
+    get(state) {
+      return (key, defaultValue) => state.config[key] ?? defaultValue;
+    },
+    defaultConfig() {
+      return {
         work: 25,
         break: 5,
         alwaysOnTop: true,
         noSound: false,
         notification: false,
-      });
+      };
+    },
+  },
+  actions: {
+    load() {
+      const config = useLocalStorage("config", this.defaultConfig);
       this.config = Object.fromEntries(Object.entries(config.value));
     },
-    set(key,value){
-      this.config = {...this.config,[key]:value};
-      useLocalStorage("config",this.config);
-    }
-  }
+    set(key, value) {
+      this.config = { ...this.config, [key]: value };
+      const config = useLocalStorage("config", this.defaultConfig);
+      config.value = this.config;
+    },
+  },
 });
